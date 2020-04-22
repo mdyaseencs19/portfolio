92cf0ecf6d9a7791b12707b0059ec9c4f028e7c1
= <=>
2020-04-23 01:44:01 +0530

    tawk.to Script added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ $(document).ready(function(){
     $('.slider').slider({full_Width:true});
 });
 
+$(document).ready(function () {
+    $("body").on("contextmenu",function(e){
+        return false;
+    });
+});
+
+//Disables Cut,Copy & Paste...
+$(document).ready(function () {
+    $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+});
+
 function toggleModal(){
     var instance = M.Modal.getInstance($('#modal3'));
     instance.open();
 }
+
